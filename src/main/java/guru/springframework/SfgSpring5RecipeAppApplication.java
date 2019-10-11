dab3572bf614165e44b8317d0c211c541bb09203
Daniel Schnetler <Daniel.Schnetler@gmail.com>
2019-10-11 09:21:15 +0200

    Spring Initializer

---
+++ src/main/java/guru/springframework/SfgSpring5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpring5RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgSpring5RecipeAppApplication.class, args);
+	}
+
+}
