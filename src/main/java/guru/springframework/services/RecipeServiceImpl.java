a9690b266cc911ea43187b4783bf8cb4ef2fc549
Daniel Schnetler <Daniel.Schnetler@gmail.com>
2019-10-17 19:38:45 +0200

    Added saveRecipeCommand

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -6,6 +6,9 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.converters.RecipeCommandToRecipe;
+import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +18,15 @@ import lombok.extern.slf4j.Slf4j;
 public class RecipeServiceImpl implements RecipeService {
 	
 	private final RecipeRepository recipeRepository;
+	private final RecipeCommandToRecipe recipeCommandToRecipe;
+	private final RecipeToRecipeCommand recipeToRecipeCommand;	
 
-	public RecipeServiceImpl(RecipeRepository recipeRepository) {
+	public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe,
+			RecipeToRecipeCommand recipeToRecipeCommand) {
 		super();
 		this.recipeRepository = recipeRepository;
+		this.recipeCommandToRecipe = recipeCommandToRecipe;
+		this.recipeToRecipeCommand = recipeToRecipeCommand;
 	}
 
 	@Override
@@ -37,6 +45,14 @@ public class RecipeServiceImpl implements RecipeService {
 			throw new RuntimeException("Recipe not found");
 		}
 		return recipeOptional.get();		
+	}
+
+	@Override
+	public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+		Recipe detatchedRecipe = recipeCommandToRecipe.convert(command);
+		Recipe savedRecipe = recipeRepository.save(detatchedRecipe);
+		log.debug("Saved RecipeId: " + savedRecipe.getId());
+		return recipeToRecipeCommand.convert(savedRecipe);
 	}
 	
 	
