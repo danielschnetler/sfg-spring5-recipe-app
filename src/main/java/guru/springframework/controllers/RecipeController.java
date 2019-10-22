069a3babb95d127c7a88c2bb1193cd8e9e37dfd9
Daniel Schnetler <Daniel.Schnetler@gmail.com>
2019-10-22 19:57:51 +0200

    Implemented Spring MVC Exception handler

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,13 +1,19 @@
 package guru.springframework.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
 import guru.springframework.commands.RecipeCommand;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -59,5 +65,16 @@ public class RecipeController {
 		return "redirect:/";
 	}
 	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ModelAndView handleNotFound() {
+		log.error("Handling not found exception");
+		
+		ModelAndView modelAndView = new ModelAndView();
+		
+		modelAndView.setViewName("404error");
+		
+		return modelAndView;
+	}
 
 }
